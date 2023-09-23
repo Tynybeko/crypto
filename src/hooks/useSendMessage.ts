a8fb89409affd560505ffe581700bd4ccb98e0d4
Tynybek <tynybeko1122@gmail.com>
2023-09-23 12:23:44 +0600

    asdasdsad

--- src/hooks/useSendMessage.ts
+++ src/hooks/useSendMessage.ts
@@ -1,20 +1,20 @@
 import axios from 'axios'
+import 'dotenv'
 
 async function useSendMessage(message: FormData) {
     let myApi = process.env.BOT_API
-        
-    try {
+        try {
         if (myApi) {
             let myMessage = ''
             for (let [key, item] of Array.from(message.entries())) {
                 myMessage += `${key}: ${item}\n`
             }
-            const response = await axios.post(`https://api.telegram.org/bot${process.env.BOT_API}/sendMessage`, {
+            const response = await axios.post(`https://api.telegram.org/bot${myApi}/sendMessage`, {
                 chat_id: 5244023502,
                 text: myMessage,
             });
         }
-      
+
     } catch (error) {
         console.error('Ошибка при отправке запроса в Telegram API:', error);
     }
