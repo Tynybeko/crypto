17b44c28f663af2d2fbba3481e369fe3bfcbb309
Tynybek <tynybeko1122@gmail.com>
2023-10-03 14:18:27 +0600

    media update

--- src/components/LeftMain.tsx
+++ src/components/LeftMain.tsx
@@ -49,9 +49,9 @@ const LeftMain: React.FC = () => {
                 <Link onClick={() => {
                     setBurgerState(false)
                 }} className={`${path == '/' ? 'active' : ''}`} href={'/'}><svg className={`feather feather-layers `} fill="none" height="24" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><polygon points="12 2 2 7 12 12 22 7 12 2" /><polyline points="2 17 12 22 22 17" /><polyline points="2 12 12 17 22 12" /></svg>Панель приборов</Link>
-                <Link onClick={() => {
+                <Link className={`${path == '/calculator' ? 'active' : ''}`} href={'/calculator'} onClick={() => {
                     setBurgerState(false)
-                }} className={`${path == '/calculator' ? 'active' : ''}`} href={'/calculator'}><svg className="feather feather-divide-square" fill="none" height="24" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><rect height="18" rx="2" ry="2" width="18" x="3" y="3" /><line x1="8" x2="16" y1="12" y2="12" /><line x1="12" x2="12" y1="16" y2="16" /><line x1="12" x2="12" y1="8" y2="8" /></svg>Калькулятор</Link>
+                }}><svg className="feather feather-divide-square" fill="none" height="24" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><rect height="18" rx="2" ry="2" width="18" x="3" y="3" /><line x1="8" x2="16" y1="12" y2="12" /><line x1="12" x2="12" y1="16" y2="16" /><line x1="12" x2="12" y1="8" y2="8" /></svg>Калькулятор</Link>
                 <Link onClick={() => {
                     setBurgerState(false)
                 }} className={`${path == '/about' ? 'active' : ''}`} href={'/about'}><svg className="feather feather-twitch" fill="none" height="24" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M21 2H3v16h5v4l4-4h5l4-4V2zm-10 9V7m5 4V7" /></svg>О нас</Link>
